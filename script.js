5e83d167241bb45da58e01a63a879615da404f9c
rafalos99 <rafsanzaniludhi@gmail.com>
2023-01-02 20:23:40 +0000

    I really do be stubborn

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-function getComputerChoice(Rock, Paper, Scissors);
+
+function getComputerChoice() {
+    let things = ['Rock', 'Paper', 'Scissor'];
+    return choice = things[Math.floor(Math.random() * things.length)];
+}
+
+function oneRound(playerSelection, computerSelection)
