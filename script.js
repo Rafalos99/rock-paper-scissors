4db7734ee9684d1e33395dafbb89ba883a580a7b
rafalos99 <rafsanzaniludhi@gmail.com>
2023-01-06 17:22:43 +0000

    update

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 
 function getComputerChoice() {
-    let things = ['Rock', 'Paper', 'Scissor'];
+    let things = ['rock', 'paper', 'scissor']; //make these lowercase so its easier
     return choice = things[Math.floor(Math.random() * things.length)];
 }
 
-function oneRound(playerSelection, computerSelection)
+getComputerChoice(); //call function to test 'choice' console after using console.log
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === 'paper') {
+        return "You lose! paper beats rock"
+    } else if (playerSelection === "rock" && computerSelection === 'scissor') {
+        return "you win!"
+    } else if (playerSelection === computerSelection) {
+        return "Draw"
+    }
+
+   
+
+}
+
+const playerSelection = "RoCk".toLowerCase(); //turned lowercase 
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
