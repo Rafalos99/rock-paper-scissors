c3c81f096ea819628baf264e9b8a1b2ee8550633
rafalos99 <rafsanzaniludhi@gmail.com>
2023-02-20 14:36:52 +0000

    adding ui elements

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const buttons = document.querySelectorAll('button');
+const resultsDiv = document.querySelector("#results");
 
 
+
+//! Computer choice
 function getComputerChoice() {
     let things = ['rock', 'paper', 'scissors']; //make these lowercase so its easier
     return choice = things[Math.floor(Math.random() * things.length)];
@@ -7,19 +11,29 @@ function getComputerChoice() {
 
 
 //! This function plays a single round
-function playRound(playerSelectionLowerCase, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if
     //define the three win states
-        ((playerSelectionLowerCase === "rock" && computerSelection === 'scissors') //rock beats scissors
-        || (playerSelectionLowerCase === "paper" && computerSelection === "rock") //paper beats rock
-        || (playerSelectionLowerCase === "scissors" && computerSelection === "paper") //scissors beats paper
+        ((playerSelection === "rock" && computerSelection === 'scissors') //rock beats scissors
+        || (playerSelection === "paper" && computerSelection === "rock") //paper beats rock
+        || (playerSelection === "scissors" && computerSelection === "paper") //scissors beats paper
         ) { return "You win!"
-    } else if (playerSelectionLowerCase === computerSelection) {
+    } else if (playerSelection === computerSelection) {
         return "Draw"
-      } else {
+    } else {
         return "You lose!"
-        }
+    }
 }
+
+//! Using buttons to choose 'rock, paper or scissors'
+buttons.forEach(button => { //loop through buttons
+  button.addEventListener('click', () => {
+    const playerSelection = button.textContent.toLowerCase(); //takes the text content of the buttons as the player selection in lower case
+    const computerSelection = getComputerChoice(); //random computer selection
+    const roundResult = playRound(playerSelection, computerSelection); //call the playround function
+    console.log(roundResult); //check 'win, draw loss' outcome
+  });
+});
 
 //! This function plays x number of rounds using the single round 'playRound' function
 //Create game function
@@ -30,19 +44,13 @@ function game(x) {
 
     // for loop to run x amount of rounds
     for (let i = 0; i < x; i++) {
-        // prompt player choice every round
-        playerSelection = prompt("Pick your choice");
-        if (playerSelection === null || playerSelection === '' ||) {
+        if (playerSelection === null || playerSelection === '' ) {
             return;
-        } else {
-            playerSelectionLowerCase = playerSelection.toLowerCase();
-        }
+        } 
 
-        // computer choice every round
-        let computerSelection = getComputerChoice();
 
         // call single game function inside loop 
-        let roundResult = playRound(playerSelectionLowerCase, computerSelection);
+        let roundResult = playRound(playerSelection, computerSelection);
 
         // if player wins then add 1 to player score
         if (roundResult === "You win!") {
@@ -53,14 +61,12 @@ function game(x) {
             computerCount += 1;
         } 
 
-        //display win,loss or draw placed into roundResult from playRound
-        console.log(roundResult);
         //display results of each round
         console.log(playerCount, computerCount);
     }
 }
 
 
-game(5);
+
 
 
